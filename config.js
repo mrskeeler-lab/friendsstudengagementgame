12572ca93a2a4b1f1b5b5fa183865b6e23bcb8cc
mrskeeler-lab <mrskeeler@gmail.com>
2026-07-24 17:46:14 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+//directions - in Github create a file and name it config.js
+//copy the content BELOW. This is where your deployment url will go each time you update the Code.gs file 
+
+const WEB_APP_URL = "YOUR_APPS_SCRIPT_URL_HERE";
+
+//
